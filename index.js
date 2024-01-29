b15b548aa60c288bf179d246d4f69e741868da0b
Mantas Navickis <navickis@gmail.com>
2024-01-29 15:38:30 +0100

    test

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const bodyParser = require('body-parser')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+app.use(bodyParser.json())
+const PORT = 3000
+const cors = require('cors')
+app.use(cors())
+
+mongoose.set('strictQuery', false)
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+app.use((req, res, next) => {
+  // Set CORS headers
+  res.header('Access-Control-Allow-Origin', '*')
+  // Add other headers as needed
+
+  // Allow all HTTP methods
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+
+  // Set the allowed headers for the preflight request
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+
+  // Continue to the next middleware
+  next()
+})
+// Routes go here
+const loginRouter = require('./routes/login')
+app.use('/login', loginRouter)
+
+const subscriberRouter = require('./routes/subscriber')
+app.use('/subscriber', subscriberRouter)
+
+const registerRouter = require('./routes/register')
+app.use('/register', registerRouter)
+
+const userRoute = require('./routes/user')
+const authenticateToken = require('./utils/auth')
+app.use('/user', authenticateToken, userRoute)
+// Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('listening for requests')
+  })
+})
