e427e2b57be0642e6d626f5b45b79f93980a8cec
Mantas Navickis <navickis@gmail.com>
2024-01-29 16:21:01 +0100

    test

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// Set 'strictQuery' option for Mongoose (to suppress the deprecation warning)
+mongoose.set('strictQuery', false)
+
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     console.log(`MongoDB Connected: ${conn.connection.host}`)
   } catch (error) {
-    console.log(error)
+    console.error(error)
     process.exit(1)
   }
 }
 
 // Routes go here
 app.all('*', (req, res) => {
-  res.json({ 'every thing': 'is awesome' })
+  res.json({ everything: 'is awesome' })
 })
 
 // Connect to the database before listening
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log('listening for requests')
+    console.log(`Server listening on port ${PORT}`)
   })
 })
